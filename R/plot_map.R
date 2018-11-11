6c121cbd33e391ca06b192182de1bcb80f741e93
rsimon64 <rsimon64@gmail.com>
2018-11-11 18:09:10 -0500

    version mejorada con upload

--- R/plot_map.R
+++ R/plot_map.R
@@ -1,16 +1,34 @@
+
+
 plot_map <- function(tbl) {
   library(magrittr)
   library(leaflet)
   m <- tbl %>% leaflet() %>%
     addTiles() %>%
-    addMarkers(lng = tbl$Lond, lat = tbl$Latd,
-               popup =
-                 paste(tbl$Codigo,
-                        tbl$Altitud,
-                        tbl$Localidad,
-                        tbl$FechaColecta,
-                        tbl$ariedad,
-                        sep = "<br/>"))
+    addCircleMarkers(
+      lng = tbl$Lond, lat = tbl$Latd,
+      radius = ~TubosColectados
+      ,
+      # # color = "red",
+      # stroke = FALSE, fillOpacity = 0.5,
+                   popup =
+                     paste(tbl$Codigo,
+                            tbl$Altitud,
+                            tbl$Localidad,
+                            tbl$FechaColecta,
+                            tbl$Variedad,
+                            sep = "<br/>")
+    )
+  # %>%
+  #
+  #   addMarkers(lng = tbl$Lond, lat = tbl$Latd,
+  #              popup =
+  #                paste(tbl$Codigo,
+  #                       tbl$Altitud,
+  #                       tbl$Localidad,
+  #                       tbl$FechaColecta,
+  #                       tbl$ariedad,
+  #                       sep = "<br/>"))
   return(m)
 }
 
